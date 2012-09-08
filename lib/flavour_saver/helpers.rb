a4526b68414b5631f8c5adc420964f2cbd37a0de
James Harton <jamesotron@gmail.com>
2012-09-08 14:46:32 +1200

    Standard handlebars helpers.

---
+++ lib/flavour_saver/helpers.rb
@@ -0,0 +1,22 @@
+module FlavourSaver
+  module Helpers
+
+    def with(argument) 
+      yield argument
+    end
+
+    def each(collection)
+      collection.each do |element|
+        yield element
+      end
+    end
+
+    def if(truthy)
+      yield if truthy
+    end
+
+    def unless(falsy, &block)
+      self.if(!falsy,&block)
+    end
+  end
+end
---
+++ spec/lib/flavour_saver/helpers_spec.rb
@@ -0,0 +1,33 @@
+require 'flavour_saver/helpers'
+
+describe FlavourSaver::Helpers do
+  subject { Object.new.tap { |o| o.extend(FlavourSaver::Helpers) } }
+
+  describe '#with' do
+    it "yields it's first argument" do
+      expect { |b| subject.with(:test,&b) }.to yield_with_args(:test)
+    end
+  end
+
+  describe '#each' do
+    it 'yeilds each element of a collection' do
+      expect { |b| subject.each([:a,:b,:c],&b) }.to yield_successive_args(:a,:b,:c)
+    end
+  end
+
+  describe '#if' do
+    it "yields if it's argument is truthy" do
+      expect { |b| subject.if(true, &b) }.to yield_control
+    end
+
+    it 'handles else'
+  end
+
+  describe '#unless' do
+    it "calls #if with it's argument negated" do
+      subject.should_receive(:if).with(false)
+      subject.unless(true)
+    end
+  end
+
+end
