822ffde08e2c3a8137b34f73ffa824eaa41f52c0
James Harton <jamesotron@gmail.com>
2012-09-10 09:57:36 +1200

    Add multi-level-helper spec.

---
+++ spec/acceptance/multi_level_helper_spec.rb
@@ -0,0 +1,21 @@
+require 'tilt'
+require 'flavour_saver'
+
+describe do
+  subject { Tilt.new(template).render(context) }
+  let(:template) { File.expand_path('../../fixtures/backtrack.hbs', __FILE__) }
+
+  it 'renders correctly when person has a name' do
+    person = Struct.new(:name).new('Alan')
+    company = Struct.new(:name).new('Rad, Inc.')
+    context = Struct.new(:person,:company).new(person, company)
+    Tilt.new(template).render(context).should == "\n  Alan\n  Rad, Inc.\n\n"
+  end
+
+  it 'renders correctly when person has no name' do
+    person = Struct.new(:name).new
+    company = Struct.new(:name).new('Rad, Inc.')
+    context = Struct.new(:person,:company).new(person, company)
+    Tilt.new(template).render(context).should == "\n  \n  Rad, Inc.\n\n"
+  end
+end
