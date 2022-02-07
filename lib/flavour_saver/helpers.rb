5c22e153932c0164b5f12928670b77b09316a81e
Lucas Arantes <lucas.ruby.dev@gmail.com>
2022-02-07 08:05:42 -0500

    Favor using `.zero?` on if/else conditions

--- lib/flavour_saver/helpers.rb
+++ lib/flavour_saver/helpers.rb
@@ -27,7 +27,7 @@ module FlavourSaver
 
       def if(truthy)
         truthy = false if truthy.respond_to?(:size) && (truthy.size == 0)
-        truthy = false if truthy == 0
+        truthy = false if truthy.respond_to?(:zero?) && truthy.zero?
 
         if truthy
           yield.contents
